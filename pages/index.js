fdcf675dbfc39646e649b04f39e6920da100ce0a
dankarger <karger.dan@gmail.com>
2022-10-18 23:58:59 +0300

    add EventItem.js + styles

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Layout from "@/components/Layout";
 import {API_URL} from "@/config/index";
+import EventItem from "@/components/EventItem";
+import Link from 'next/link'
 
 export default function Home({events}) {
-
   return (
     <Layout >
       <h1>Upcoming Events</h1>
-
+        {events.length ===0 && <h3>No Event to show</h3>}
+        {events.map(evt=>(
+            <EventItem key={evt.id} evt={evt} />
+        ))}
+        {events.length>0 && (
+            <Link href={'/events'}>
+                <a className='btn-secondary'>View All Events</a>
+            </Link>
+        )}
     </Layout>
   )
 }
@@ -16,7 +25,7 @@ export async function getStaticProps() {
     const events = await res.json();
 
     return {
-        props: { events },
+        props: { events:events.slice(0,3) },
         revalidate: 1
     }
 }
